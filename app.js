b57ebf02ebcc9c08174e26371f133eae2ae9f23c
soooooom <abc098757@gmail.com>
2020-03-07 19:01:56 +0900

    add views

--- app.js
+++ app.js
@@ -3,26 +3,29 @@ import morgan from "morgan";
 import helmet from "helmet"; 
 import bodyParser from "body-parser"; 
 import cookieParser from "cookie-parser"; 
+import { localMiddleware } from './middleware';
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-//import { userRouter } from "./router";
 import routes from "./routes";
+
+
+
 const app = express();
 
+app.set("view engine", "pug");
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true})); 
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localMiddleware)
 
 app.use('/',globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
-// app.use('/users', userRouter);
-// user 접근하면 userRouter 사용하겠다
+
 export default app;
-// import 시 app object 주겠다
 
 
-//m(data)v(data모습)c(data찾는함수)
+
